880dc3b1be865e74c2674a6d937c898d6d34bf0d
Stephen Rodriguez <steprodriguez10@gmail.com>
2018-05-05 23:15:03 +0200

    initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,18 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "go-coverage-viewer" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let disposable = vscode.commands.registerCommand('extension.generateCoverage', () => {
+        vscode.window.showInformationMessage('Generating Coverage Results...');
+        // 1. Get the path to the current file
+        var editor = vscode.window.activeTextEditor
+        // 2. Determine the Go Package name
+        // 3. If package name is main, do not generate
+        // 4. Run command to generate coverage and output to temp folder
+        // 5. Open WebView with contents of coverage file
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
